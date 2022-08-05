f975839113875c9c8bf01da3b20e43c22cfb17a2
Peter Vang <vang_peter06@yahoo.com>
2022-08-05 09:04:19 -0700

    add engineer model

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,20 @@
+using System.Collection.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public Engineer()
+    {
+      this.joinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public int EngineerId { get; set; }
+    public string Name { get; set; }
+    [DisplayFormat(DataFormatString = "0:MM/DD/YYYY")]
+    public DateTime HireDate { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
+  }
+}
