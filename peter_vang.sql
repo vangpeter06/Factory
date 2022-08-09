2a6297355c2fd01f800b650c51d8686e91b8a5a3
Peter Vang <vang_peter06@yahoo.com>
2022-08-08 22:14:24 -0700

    add sql to git

--- peter_vang.sql
+++ peter_vang.sql
@@ -16,53 +16,82 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `clients`
+-- Table structure for table `EngineerMachine`
 --
 
-DROP TABLE IF EXISTS `clients`;
+DROP TABLE IF EXISTS `EngineerMachine`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `clients` (
-  `ClientId` int NOT NULL AUTO_INCREMENT,
-  `Name` varchar(255) DEFAULT NULL,
-  `PhoneNumber` varchar(255) DEFAULT NULL,
-  `StyleHair` varchar(255) DEFAULT NULL,
-  `StylistId` int DEFAULT '0',
-  PRIMARY KEY (`ClientId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `EngineerMachine` (
+  `EngineerMachineId` int NOT NULL AUTO_INCREMENT,
+  `EngineerId` int NOT NULL,
+  `MachineId` int NOT NULL,
+  PRIMARY KEY (`EngineerMachineId`),
+  KEY `IX_EngineerMachine_EngineerId` (`EngineerId`),
+  KEY `IX_EngineerMachine_MachineId` (`MachineId`),
+  CONSTRAINT `FK_EngineerMachine_Engineers_EngineerId` FOREIGN KEY (`EngineerId`) REFERENCES `Engineers` (`EngineerId`) ON DELETE CASCADE,
+  CONSTRAINT `FK_EngineerMachine_Machines_MachineId` FOREIGN KEY (`MachineId`) REFERENCES `Machines` (`MachineId`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `clients`
+-- Dumping data for table `EngineerMachine`
 --
 
-LOCK TABLES `clients` WRITE;
-/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
-/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+LOCK TABLES `EngineerMachine` WRITE;
+/*!40000 ALTER TABLE `EngineerMachine` DISABLE KEYS */;
+INSERT INTO `EngineerMachine` VALUES (4,2,2),(5,1,2);
+/*!40000 ALTER TABLE `EngineerMachine` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `stylists`
+-- Table structure for table `Engineers`
 --
 
-DROP TABLE IF EXISTS `stylists`;
+DROP TABLE IF EXISTS `Engineers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `stylists` (
-  `StylistId` int NOT NULL AUTO_INCREMENT,
-  `Name` varchar(255) DEFAULT NULL,
+CREATE TABLE `Engineers` (
+  `EngineerId` int NOT NULL AUTO_INCREMENT,
+  `Name` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `HireDate` datetime DEFAULT NULL,
-  PRIMARY KEY (`StylistId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`EngineerId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `stylists`
+-- Dumping data for table `Engineers`
 --
 
-LOCK TABLES `stylists` WRITE;
-/*!40000 ALTER TABLE `stylists` DISABLE KEYS */;
-/*!40000 ALTER TABLE `stylists` ENABLE KEYS */;
+LOCK TABLES `Engineers` WRITE;
+/*!40000 ALTER TABLE `Engineers` DISABLE KEYS */;
+INSERT INTO `Engineers` VALUES (1,'joe','2022-08-01 00:00:00'),(2,'ruth','2022-08-02 00:00:00');
+/*!40000 ALTER TABLE `Engineers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Machines`
+--
+
+DROP TABLE IF EXISTS `Machines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Machines` (
+  `MachineId` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `InstallDate` datetime DEFAULT NULL,
+  PRIMARY KEY (`MachineId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Machines`
+--
+
+LOCK TABLES `Machines` WRITE;
+/*!40000 ALTER TABLE `Machines` DISABLE KEYS */;
+INSERT INTO `Machines` VALUES (2,'3d',NULL);
+/*!40000 ALTER TABLE `Machines` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -74,4 +103,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-29 13:30:25
+-- Dump completed on 2022-08-08 13:06:41
