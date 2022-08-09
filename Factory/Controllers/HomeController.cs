f3fdd7cdb97c7e68f0cac9936aabdf28f7f7901d
Peter Vang <vang_peter06@yahoo.com>
2022-08-08 22:12:05 -0700

    add home page to see engineer and machine

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Factory.Models;
 
 namespace Factory.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly FactoryContext _db;
+
+      public HomeController(FactoryContext db)
+      {
+        _db = db;
+      }
 
       [HttpGet("/")]
       public ActionResult Index()
       {
+        ViewBag.EngineerInventory = new List<Engineer>(_db.Engineers.OrderBy(engineer => engineer.Name));
+        ViewBag.MachineInventory = new List<Machine>(_db.Machines.OrderBy(machine => machine.Name));
         return View();
       }
 
